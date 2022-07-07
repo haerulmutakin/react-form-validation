4bddb3063f8e87ee9c2d0a092672e90cf8ec1cf7
haerulmutakin <haerulm.takin@gmail.com>
2022-07-07 11:40:30 +0700

    updates

--- src/HookForm.js
+++ src/HookForm.js
@@ -1,43 +1,65 @@
+import { useForm } from 'react-hook-form';
+
 const HookForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    console.log(errors);
     return ( 
         <div>
             <h1>Sign Up Form</h1>
-            <form>
+            <form onSubmit={handleSubmit((data) => {
+                console.log(data);
+            })}>
                 <div className="form-group">
                     <label className="label-required">Username</label>
                     <input
                         type="text"
-                        name="username"
+                        {...register('username', {
+                            required: 'This field is required',
+                            maxLength: {value: 5, message: 'Exceeded maximum length'}
+                        })}
                     />
+                    {errors.username && <div className="error">{errors.username.message}</div>}
                 </div>
                 <div className="form-group">
                     <label className="label-required">Email</label>
                     <input
                         type="text"
-                        name="email"
+                        {...register('email', {
+                            required: 'This field is required'
+                        })}
                     />
+                    {errors.email && <div className="error">{errors.email.message}</div>}
                 </div>
                 <div className="form-group">
                     <label className="label-required">Password</label>
                     <input
                         type="password"
-                        name="password"
+                        {...register('password', {
+                            required: 'This field is required'
+                        })}
                     />
+                    {errors.password && <div className="error">{errors.password.message}</div>}
                 </div>
                 <div className="form-group">
                     <label className="label-required">Confirm Password</label>
                     <input 
                         type="password"
-                        name="confirmPassword"
+                        {...register('confirmPassword', {
+                            required: 'This field is required'
+                        })}
                     />
+                    {errors.confirmPassword && <div className="error">{errors.confirmPassword.message}</div>}
                 </div>
                 <div className="form-group">
                     <input
                         id="agreement"
                         type="checkbox"
-                        name="agreement"
+                        {...register('agreement', {
+                            required: 'This field is required'
+                        })}
                         />
                     <label htmlFor="agreement" className="label-required">I agree to Terms of Services</label>
+                    {errors.agreement && <div className="error">{errors.agreement.message}</div>}
                 </div>
                 <button type="submit">Sign Up</button>
             </form>
